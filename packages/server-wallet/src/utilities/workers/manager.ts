22ed5a0aec8106555ddd63757c84d808b54055cb
Alex <alex.gap@consensys.net>
2020-09-16 17:16:45 -0700

    Try threads.js

--- packages/server-wallet/src/utilities/workers/manager.ts
+++ packages/server-wallet/src/utilities/workers/manager.ts
@@ -1,38 +1,18 @@
-import {Worker} from 'worker_threads';
-import path from 'path';
-
-import {Pool} from 'tarn';
+import {Pool, spawn, Worker} from 'threads';
 import {State, StateWithHash} from '@statechannels/wallet-core';
 
-import {StateChannelWorkerData} from './worker-message';
-
 export class WorkerManager {
-  pool = new Pool({
-    create: (): Worker => new Worker(path.resolve(__dirname, './loader.js')),
-    destroy: (worker: Worker): Promise<number> => worker.terminate(),
-    min: 1,
-    max: 5,
-    reapIntervalMillis: 2147483647,
-  });
+  private pool = Pool(() => spawn(new Worker('./worker')));
 
   public async concurrentSignState(
     state: StateWithHash,
     privateKey: string
   ): Promise<{state: State; signature: string}> {
-    const worker = await this.pool.acquire().promise;
-    const data: StateChannelWorkerData = {operation: 'SignState', state, privateKey};
-    const resultPromise = new Promise<{state: State; signature: string}>(resolve =>
-      worker.once('message', (response: {state: State; signature: string}) => {
-        this.pool.release(worker);
-        resolve(response);
-      })
-    );
-
-    worker.postMessage(data);
-    return resultPromise;
+    const signature = await this.pool.queue(async sign => sign(state.stateHash, privateKey));
+    return {state, signature};
   }
 
   public async destroy(): Promise<void> {
-    await this.pool.destroy();
+    await this.pool.terminate();
   }
 }
--- packages/server-wallet/src/utilities/workers/worker.ts
+++ packages/server-wallet/src/utilities/workers/worker.ts
@@ -1,29 +1,7 @@
-import {parentPort, isMainThread} from 'worker_threads';
+import {expose} from 'threads/worker';
 
-import {hashState} from '@statechannels/wallet-core';
+import {fastSignData} from '../signatures';
 
-import {fastRecoverAddress, fastSignState} from '../signatures';
-
-import {isStateChannelWorkerData} from './worker-message';
-
-parentPort?.on('message', async (message: any) => {
-  if (isMainThread) {
-    throw new Error('Attempting to execute worker thread script on the main thread');
-  }
-
-  if (!isStateChannelWorkerData(message)) {
-    throw new Error('Incorrect worker data');
-  }
-  switch (message.operation) {
-    case 'HashState ':
-      parentPort?.postMessage(await hashState(message.state));
-      break;
-    case 'RecoverAddress':
-      parentPort?.postMessage(
-        await fastRecoverAddress(message.state, message.signature, message.state.stateHash)
-      );
-      break;
-    case 'SignState':
-      parentPort?.postMessage(await fastSignState(message.state, message.privateKey));
-  }
+expose(async function sign(stateHash: string, privateKey: string) {
+  return fastSignData(stateHash, privateKey);
 });
