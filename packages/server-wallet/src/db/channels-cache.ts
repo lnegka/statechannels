bec2d841ff0823de6565540ee54e5bd5aff79e4a
Andrew Stewart <andrew.gord.stewart@gmail.com>
2020-09-21 17:00:21 -0700

    --wip-- [skip ci]

--- packages/server-wallet/src/db/channels-cache.ts
+++ packages/server-wallet/src/db/channels-cache.ts
@@ -10,8 +10,8 @@ const TABLE = `${SCHEMA}.payment_channels`;
 const channelCache = knex.table(TABLE);
 
 class ChannelManagementAPI {
-  static async putChannel(channel_id: string, group_id: string, turn_number = 3): Promise<void> {
-    await channelCache.insert({channel_id, group_id, turn_number});
+  static async putChannel(channel_id: string, context_id: string, turn_number = 3): Promise<void> {
+    await channelCache.insert({channel_id, context_id, turn_number});
   }
 
   static async deleteChannel(channel_id: string): Promise<void> {
@@ -35,7 +35,7 @@ class ChannelManagementAPI {
 
 class PaymentChannelAccessAPI {
   static async acquirePaymentChannel(
-    group_id: string
+    context_id: string
   ): Promise<{channelId: string; turnNumber: number; tx: Transaction}> {
     const tx = await knex.transaction();
 
@@ -43,13 +43,13 @@ class PaymentChannelAccessAPI {
       await tx.raw(
         `
         SELECT * FROM ??
-        WHERE group_id = ?
+        WHERE context_id = ?
         AND turn_number % 2 = 1
         LIMIT 1 
         FOR UPDATE
         SKIP LOCKED;
         `,
-        [TABLE, group_id]
+        [TABLE, context_id]
       )
     ).rows[0];
 
@@ -92,9 +92,11 @@ class PaymentChannelAccessAPI {
 
 const delay = async (num = 10): Promise<void> => new Promise(resolve => setTimeout(resolve, num)); // 3 sec
 
-async function acquireAndHold(groupId: string, id: number): Promise<string> {
+async function acquireAndHold(contextId: string, id: number): Promise<string> {
   console.log(`${id}: acquiring`);
-  const {channelId, turnNumber, tx} = await PaymentChannelAccessAPI.acquirePaymentChannel(groupId);
+  const {channelId, turnNumber, tx} = await PaymentChannelAccessAPI.acquirePaymentChannel(
+    contextId
+  );
   console.log(`${id}: acquired`);
 
   await delay();
@@ -107,25 +109,25 @@ async function acquireAndHold(groupId: string, id: number): Promise<string> {
 
 async function run1(): Promise<void> {
   const newInsert = ethers.Wallet.createRandom().address;
-  const group5 = 'group-5';
-  await channelCache.delete().where({group_id: group5});
+  const context5 = 'context-5';
+  await channelCache.delete().where({context_id: context5});
 
-  await ChannelManagementAPI.putChannel(newInsert, group5, 5);
+  await ChannelManagementAPI.putChannel(newInsert, context5, 5);
 
   let id = 0;
-  const channelId = await acquireAndHold(group5, (id += 1));
-  await acquireAndHold(group5, (id += 1)).catch(reason =>
+  const channelId = await acquireAndHold(context5, (id += 1));
+  await acquireAndHold(context5, (id += 1)).catch(reason =>
     console.log(`Expected to fail; failed with ${reason}`)
   );
 
-  console.log(`got ${channelId} for group 5. Expected ${newInsert}`);
+  console.log(`got ${channelId} for context 5. Expected ${newInsert}`);
 
-  const group3 = 'group-3';
+  const context3 = 'context-3';
   const acquiredChannels = await Promise.all(
-    _.range(4).map(async () => acquireAndHold(group3, (id += 1)))
+    _.range(4).map(async () => acquireAndHold(context3, (id += 1)))
   );
 
-  console.log(`acquired ${acquiredChannels} for group 3`);
+  console.log(`acquired ${acquiredChannels} for context 3`);
 }
 
 async function run2(): Promise<void> {
@@ -135,11 +137,11 @@ async function run2(): Promise<void> {
 }
 
 async function seed(): Promise<void> {
-  const groups = _.range(10).map(i => `group-${i}`);
+  const contexts = _.range(10).map(i => `context-${i}`);
   const rows = () =>
     _.range(20_000).map(i => ({
       channel_id: `channel-${i}-${_.random(12345678910111213.1).toString()}`,
-      group_id: `group-${_.random(0, 1000)}`,
+      context_id: `context-${_.random(0, 1000)}`,
       turn_number: 3,
     }));
 
@@ -154,7 +156,7 @@ async function seed(): Promise<void> {
 async function benchmark(): Promise<void> {
   await Promise.all(
     _.range(500).map(async i => {
-      const {tx} = await PaymentChannelAccessAPI.acquirePaymentChannel('group-3');
+      const {tx} = await PaymentChannelAccessAPI.acquirePaymentChannel('context-3');
       await tx.commit();
     })
   );
@@ -165,7 +167,7 @@ async function benchmark(): Promise<void> {
     _.range(NUM_CHANNELs).map(async i => {
       let key: string;
       // console.time((key = `${i}  get channel`));
-      const {tx} = await PaymentChannelAccessAPI.acquirePaymentChannel('group-3');
+      const {tx} = await PaymentChannelAccessAPI.acquirePaymentChannel('context-3');
       // console.timeEnd(key);
 
       // console.time((key = `${i}  commit`));
