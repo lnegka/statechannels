e58ab65e54c5586cc3c9ddcb5a3f2e50df5af5e6
Alex Gap <alex.gap@mesh.xyz>
2020-09-22 18:19:24 -0700

    try built

--- packages/server-wallet/src/utilities/workers/loader.js
+++ packages/server-wallet/src/utilities/workers/loader.js
@@ -3,11 +3,21 @@ const fs = require('fs');
 
 const TS_WORKER_PATH = path.resolve(__dirname, './worker.ts');
 const JS_WORKER_PATH = path.resolve(__dirname, './worker.js');
-if (fs.existsSync(TS_WORKER_PATH)) {
+const JS_BUILD_WORKER_PATH = path.resolve(
+  __dirname,
+  '../../../lib/src/utilities/workers/worker.js'
+);
+console.log('sanity');
+if (fs.existsSync(JS_WORKER_PATH)) {
+  console.log('js worker');
+  require(JS_WORKER_PATH);
+} else if (fs.existsSync(JS_BUILD_WORKER_PATH)) {
+  console.log('buildt js worker');
+  require(JS_BUILD_WORKER_PATH);
+} else if (fs.existsSync(TS_WORKER_PATH)) {
+  console.warn('Using ts-node to load the script');
   require('ts-node').register({typeCheck: false});
   require(TS_WORKER_PATH);
-} else if (fs.existsSync(JS_WORKER_PATH)) {
-  require(JS_WORKER_PATH);
 } else {
   throw new Error('Could not find worker.ts or worker.js');
 }
