e97326e1aedbb1f03c0708cc862710b045b71335
geoknee <georgeknee@googlemail.com>
2020-10-20 13:40:41 +0100

    WIP add test for bulkCreateAndLedgerFund

---
+++ packages/server-wallet/src/__test__/bulk-create-and-ledger-fund.test.ts
@@ -0,0 +1,68 @@
+import {CreateChannelParams, Participant, Allocation} from '@statechannels/client-api-schema';
+import {makeDestination} from '@statechannels/wallet-core';
+import {BigNumber, ethers} from 'ethers';
+
+import {defaultTestConfig} from '../../config';
+import {Wallet} from '../../wallet';
+
+const NUMBER_OF_CHANNELS = 100;
+const a = new Wallet({...defaultTestConfig, postgresDBName: 'TEST_A'});
+const b = new Wallet({...defaultTestConfig, postgresDBName: 'TEST_B'});
+
+let channelId: string;
+let participantA: Participant;
+let participantB: Participant;
+
+beforeAll(async () => {
+  await a.dbAdmin().createDB();
+  await b.dbAdmin().createDB();
+  await Promise.all([a.dbAdmin().migrateDB(), b.dbAdmin().migrateDB()]);
+
+  participantA = {
+    signingAddress: await a.getSigningAddress(),
+    participantId: 'a',
+    destination: makeDestination(
+      '0xaaaa000000000000000000000000000000000000000000000000000000000001'
+    ),
+  };
+  participantB = {
+    signingAddress: await b.getSigningAddress(),
+    participantId: 'b',
+    destination: makeDestination(
+      '0xbbbb000000000000000000000000000000000000000000000000000000000002'
+    ),
+  };
+});
+afterAll(async () => {
+  await Promise.all([a.destroy(), b.destroy()]);
+  await a.dbAdmin().dropDB();
+  await b.dbAdmin().dropDB();
+});
+
+it('Creates 100 channels between 2 wallet sand ledger funds them ', async () => {
+  const allocation: Allocation = {
+    allocationItems: [
+      {
+        destination: participantA.destination,
+        amount: BigNumber.from(1).toHexString(),
+      },
+      {
+        destination: participantB.destination,
+        amount: BigNumber.from(1).toHexString(),
+      },
+    ],
+    token: '0x00', // must be even length
+  };
+
+  const createChannelParams: CreateChannelParams = {
+    participants: [participantA, participantB],
+    allocations: [allocation],
+    appDefinition: ethers.constants.AddressZero,
+    appData: '0x00', // must be even length
+    fundingStrategy: 'Direct',
+  };
+
+  const resultA0 = await a.bulkCreateAndLedgerFund(createChannelParams, NUMBER_OF_CHANNELS);
+
+  expect(resultA0).toMatchObject({ledgerId: 'foo', channelIds: []});
+});
